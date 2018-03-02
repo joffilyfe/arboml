c931ff62d754912bc77ad8560cab98707a8816bf
Joffily Ferreira <joffily@outlook.com>
2018-03-02 12:31:45 -0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,62 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">ArboML</h1>
-          <h2 className="App-subtitle">Identifique Dengue ou Chikungunya por meio de sintomas</h2>
+      <div class="App">
+        <header class="App-header">
+          <h1 class="header-title">ArboML</h1>
+          <h2 class="header-subtitle">Identifique Dengue ou Chikungunya por meio de sintomas</h2>
         </header>
-        <div className="Questions">
 
-          <div className="Question">
-            <label>Você tem febre alta?</label>
-            <label><input type="radio" name="febre" value="1"/>Sim</label>
-            <label><input type="radio" name="febre" value="0"/>Não</label>
+        <main class="App-content">
+          <div class="Question">
+            <h3 class="Question-title">Você concorda com isso?</h3>
+            <div class="Answers">
+              <button class="answer answer-no">Não</button>
+              <button class="answer answer-yes">Sim</button>
+            </div>
           </div>
-
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <div className="Question">
-            <label>Você tem dor de cabeça?</label>
-            <label><input type="radio" name="enxantema" value="1"/>Sim</label>
-            <label><input type="radio" name="enxantema" value="0"/>Não</label>
-          </div>
-
-          <button>Ver</button>
-        </div>
+        </main>
       </div>
     );
   }
